b7a767a52c3ae4a1b1cf534763b17eaf76e7a237
Alessandro Dilevrano <57458926+v3l1d@users.noreply.github.com>
2024-05-04 10:56:08 +0200

    Introduced collector service

---
+++ collectorservice/src/main/java/com/thesis/collectorservice/CollectorserviceApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.collectorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorserviceApplication.class, args);
+	}
+
+}
---
+++ collectorservice/src/test/java/com/thesis/collectorservice/CollectorserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis.collectorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
